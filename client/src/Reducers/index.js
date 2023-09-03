1622246d31a568c7b1c2b0e4aaf622e1236c3eca
Yuvraj Thapa <yuvrajthapa692@gmail.com>
2023-09-03 15:10:08 +0530

    Added Initial Setup Of Redux Form

--- client/src/Reducers/index.js
+++ client/src/Reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux"; // TO combine all the available reducers
+
 import AuthReducers from "./AuthReducers";
+import { reducer as formReducer } from "redux-form";
+
 export default combineReducers({
-  //   replaceMe: () => 5,
   auth: AuthReducers,
+  form: formReducer,
 });
