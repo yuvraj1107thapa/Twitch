1a00d2ce10498e7c294cea5efe1e0736d3eaa16a
Yuvraj Thapa <yuvrajthapa692@gmail.com>
2023-09-03 13:17:47 +0530

    Added Semantic-UI and Header component

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,17 +1,20 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom"; // To implement React-Router Functionalities
+import { BrowserRouter, Route } from "react-router-dom";
 
 import StreamList from "./streams/StreamList";
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
+import "../CSS/index.css";
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="parent-div ui container">
         <BrowserRouter>
           <div>
+            <Header />
             <Route path="/" exact component={StreamList} />
             <Route path="/streams/new" exact component={StreamCreate} />
             <Route path="/streams/edit" exact component={StreamEdit} />
